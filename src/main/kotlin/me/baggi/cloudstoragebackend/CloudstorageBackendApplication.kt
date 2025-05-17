05101796776b025d81f02892f39905aa8502e020
inotbaggi <inotbaggi@gmail.com>
2025-05-17 10:14:09 +0300

    Initial commit

---
+++ src/main/kotlin/me/baggi/cloudstoragebackend/CloudstorageBackendApplication.kt
@@ -0,0 +1,11 @@
+package me.baggi.cloudstoragebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CloudstorageBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<CloudstorageBackendApplication>(*args)
+}
---
+++ src/test/kotlin/me/baggi/cloudstoragebackend/CloudstorageBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.baggi.cloudstoragebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CloudstorageBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
