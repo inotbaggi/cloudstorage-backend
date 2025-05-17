4486821bf4cd11969d854726d1bc0b1bc396d857
inotbaggi <inotbaggi@gmail.com>
2025-05-17 10:55:29 +0300

    forgot exception handling in security config

--- src/main/kotlin/me/baggi/cloud/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/me/baggi/cloud/configuration/SecurityConfiguration.kt
@@ -3,12 +3,14 @@ package me.baggi.cloud.configuration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
 
 @Configuration
 @EnableWebSecurity
@@ -28,6 +30,7 @@ class SecurityConfiguration {
                 ).permitAll()
                     .requestMatchers("/api/**").authenticated()
             }
+            .exceptionHandling { it.authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)) }
             .build()
 
     @Bean
